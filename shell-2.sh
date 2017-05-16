f499c5127fa54e4341652124f251b1291bfaa8c9
Huichao Zhao <zhaohc10@users.noreply.github.com>
2017-05-16 12:27:26 -0700

    Create shell-2.sh

---
+++ shell-2.sh
@@ -0,0 +1,99 @@
+http://c.biancheng.net/cpp/view/2737.html
+#########################################################
+
+$0	当前脚本的文件名
+$n	传递给脚本或函数的参数。n 是一个数字，表示第几个参数。例如，第一个参数是$1，第二个参数是$2。
+$#	传递给脚本或函数的参数个数。
+$*	传递给脚本或函数的所有参数。
+$@	传递给脚本或函数的所有参数。
+	$* 和 $@ 都表示传递给函数或脚本的所有参数，
+	不被双引号(" ")包含时，都以"$1" "$2" … "$n" 的形式输出所有参数。
+	但是当它们被双引号(" ")包含时，"$*" 会将所有的参数作为一个整体，以"$1 $2 … $n"的形式输出所有参数；"$@" 会将各个参数分开，以"$1" "$2" … "$n" 的形式输出所有参数。
+$?	上个命令的退出状态，或函数的返回值。所谓退出状态，就是上一个命令执行后的返回结果。退出状态是一个数字，一般情况下，大部分命令执行成功会返回 0，失败返回 1。
+$$	当前Shell进程ID。对于 Shell 脚本，就是这些脚本所在的进程ID。
+
+#########################################################
+
+使用 unset 命令可以删除变量。语法：  unset variable_name
+
+${var}			变量本来的值
+${var:-word}	如果变量 var 为空或已被删除(unset)，那么返回 word，但不改变 var 的值。
+${var:=word}	如果变量 var 为空或已被删除(unset)，那么返回 word，并将 var 的值设置为 word。
+${var:?message}	如果变量 var 为空或已被删除(unset)，那么将消息 message 送到标准错误输出，可以用来检测变量 var 是否可以被正常赋值。
+若此替换出现在Shell脚本中，那么脚本将停止运行。
+${var:+word}	如果变量 var 被定义，那么返回 word，但不改变 var 的值。
+
+#########################################################
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "First Index: ${NAME[0]}"
+echo "Second Index: ${NAME[1]}"
+使用@ 或 * 可以获取数组中的所有元素，例如：
+
+echo "First Method: ${NAME[*]}"   # First Method: Zara Qadir Mahnaz Ayan Daisy
+
+
+#########################################################
+
+重定向深入讲解
+
+一般情况下，每个 Unix/Linux 命令运行时都会打开三个文件：
+标准输入文件(stdin)：stdin的文件描述符为0，Unix程序默认从stdin读取数据。
+标准输出文件(stdout)：stdout 的文件描述符为1，Unix程序默认向stdout输出数据。
+标准错误文件(stderr)：stderr的文件描述符为2，Unix程序会向stderr流中写入错误信息。
+
+默认情况下，command > file 将 stdout 重定向到 file，command < file 将stdin 重定向到 file。
+
+
+command > file	将输出重定向到 file。
+command < file	将输入重定向到 file。
+command >> file	将输出以追加的方式重定向到 file。
+n > file	将文件描述符为 n 的文件重定向到 file。
+n >> file	将文件描述符为 n 的文件以追加的方式重定向到 file。
+n >& m	将输出文件 m 和 n 合并。
+n <& m	将输入文件 m 和 n 合并。
+<< tag	将开始标记 tag 和结束标记 tag 之间的内容作为输入。
+
+如果希望将 stdout 和 stderr 合并后重定向到 file，可以这样写：
+$command > file 2>&1
+
+如果希望执行某个命令，但又不希望在屏幕上显示输出结果，那么可以将输出重定向到 /dev/null：
+$ command > /dev/null
+	/dev/null 是一个特殊的文件，写入到它的内容都会被丢弃；如果尝试从该文件读取内容，那么什么也读不到。但是 /dev/null 文件非常有用，将命令的输出重定向到它，会起到”禁止输出“的效果。
+#########################################################
+Here Document
+
+Here Document 目前没有统一的翻译，这里暂译为”嵌入文档“。Here Document 是 Shell 中的一种特殊的重定向方式，它的基本的形式如下：
+command << delimiter
+    document
+delimiter
+它的作用是将两个 delimiter 之间的内容(document) 作为输入传递给 command。
+
+注意：
+结尾的delimiter 一定要顶格写，前面不能有任何字符，后面也不能有任何字符，包括空格和 tab 缩进。
+开始的delimiter前后的空格会被忽略掉。
+
+***************************************
+wc -l << EOF
+    This is a simple lookup program
+    for good (and bad) restaurants
+    in Cape Town.
+EOF
+————————————————————————————————————————
+输出为 3
+***************************************
+cat << EOF
+This is a simple lookup program
+for good (and bad) restaurants
+in Cape Town.
+EOF
+————————————————————————————————————————
+输出为： 
+This is a simple lookup program
+for good (and bad) restaurants
+in Cape Town.
+***************************************
+
